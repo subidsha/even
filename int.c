13fef22e73cae4904efd3c1a4559aedc019394b0
subidsha <31504698+subidsha@users.noreply.github.com>
2018-01-18 10:45:44 +0000

    Create int.c

---
+++ int.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,n=5,bit=0,n,k;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<=n;i++)
+	{
+	printf("%d",i);
+	}
+	for(i=0;i<=k;i++)
+	{
+		printf("%d",i);
+		
+	}
+	bit=bit+a[i];
+	printf("sum of first two integer",n,k);
+	return 0;
+	
+}
