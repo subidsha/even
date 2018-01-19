e8f1651cc3255e5ea2f21871e314fc4b8c9634d0
subidsha <31504698+subidsha@users.noreply.github.com>
2018-01-19 16:58:14 +0530

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,rem, i=0,a;
+	scanf("%d",&n);
+	a=n;
+	while(a!=0)
+	{
+		rem=n%10;
+		i+=rem*rem*rem;
+		a/=10;
+		
+	}
+	if(i==0)
+		printf("\n armstrong",n);
+		
+	else
+		printf("\n not armstrong",n);
+}
