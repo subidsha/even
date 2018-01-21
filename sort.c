7a93b254fecca9a918b2e795942ac5e3a3ff37bb
subidsha <31504698+subidsha@users.noreply.github.com>
2018-01-21 15:38:09 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int n[10],i,j,temp,a;
+	for(a=1;a<=5;a++)
+	scanf("%d\t",&n[a]);
+	for(i=1;i<=5;i++)
+	{
+	for(j=i+1;j<=5;j++)
+	{
+		if(n[i]>n[j])
+		{
+		temp=n[i];
+		n[i]=n[j];
+		n[j]=temp;
+		}
+	}
+	}
+	for(a=1;a<=5;a++)
+	{
+	printf("%d\t" ,n[a]);
+	}
+	
+	
+	
+
+	}
+	
