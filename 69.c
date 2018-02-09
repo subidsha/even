9cb85cbc051bd2994657a8e59a67b318e1daa269
subidsha <31504698+subidsha@users.noreply.github.com>
2018-02-09 14:53:54 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d\t%d",&a,&b);
+    if(a==b)
+    {
+    	printf("even");
+    }
+    else
+    {
+    	printf("odd");
+    }
+    return 0;
+} 
+   
