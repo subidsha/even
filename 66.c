6ecdc3637f204e47bc4e2705a501bd94ad2a0efb
subidsha <31504698+subidsha@users.noreply.github.com>
2018-02-09 14:43:36 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a,i,count=0,c;
+	scanf("%d",&a);
+	for(i=1;i<=a;i++)
+	{
+		if(a%i==0)
+		c++;
+	}
+	if(c==2)
+	{
+		printf("prime");
+	}
+	else
+	{
+		printf("not prime");
+	}
+	return 0;
+}
