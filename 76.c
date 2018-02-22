b66be8aa553a97e44d8bc212cb482f5c8b07dcfd
subidsha <31504698+subidsha@users.noreply.github.com>
2018-02-22 16:24:00 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,c=0;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	if(n%i==0)
+	{
+	c++;
+	}
+	}
+	if(c!=2)
+	{
+		printf("composite");
+	}
+	else
+	{
+		printf("not composite");
+	}
+  return 0;
+	
+}
