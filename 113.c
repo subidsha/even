1fa9fcb3700a18f1880d6a6c90648a456baee0a8
subidsha <31504698+subidsha@users.noreply.github.com>
2018-03-01 16:24:42 +0530

    Create 113.c

---
+++ 113.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+    int n,k,i,a[10],count=0;
+    scanf("%d\t%d",&n,&k);
+    for(i=0;i<=n;i++)
+    scanf("%d\t",&a[i]);
+    for(i=0;i<=n;i++)
+    {
+    if(a[i]==k)
+    {
+    	count++;
+    }
+    }
+    printf("%d",count);
+    return 0;
+}
