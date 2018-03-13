e9cfd3fc9d0c45aef09700220ea2b0bf7c426f04
subidsha <31504698+subidsha@users.noreply.github.com>
2018-03-13 15:52:02 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[100];
+	int l,i,c=0;
+	scanf("%[^\t\n]s",&a);
+	l=strlen(a);
+	for(i=0;i<=l;i++)
+	{
+	if(a[i]==' ')
+	{
+	c++;
+	}
+	}
+	printf("%d",c);
+	return 0;
+	
+}
